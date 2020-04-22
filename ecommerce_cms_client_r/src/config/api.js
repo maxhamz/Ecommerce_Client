fa18387c423ce3a20760a133777b556eae2f7451
maxhamz <maxwell.hamzah@gmail.com>
2020-04-23 00:44:06 +0700

    socketed admin on product checkouts

--- ecommerce_cms_client_r/src/config/api.js
+++ ecommerce_cms_client_r/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const BASEURL = 'http://localhost:3100'
-// const BASEURL = 'https://ecommerce-server-2.herokuapp.com'
+// const BASEURL = 'http://localhost:3100'
+const BASEURL = 'https://ecommerce-server-2.herokuapp.com'
 
 const instance = axios.create({
   baseURL: BASEURL
